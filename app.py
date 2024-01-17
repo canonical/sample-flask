9d7fdb705667c177cc16e31572e3dcef2b7b5beb
Weii Wang <weii.wang@canonical.com>
2024-01-17 11:31:42 +0800

    Add translations

--- app.py
+++ app.py
@@ -4,10 +4,22 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_prefixed_env()
 
+TRANSLATIONS = {
+    "ara": "مرحبا",
+    "eng": "Hello",
+    "fra": "Bonjour",
+    "hin": "नमस्ते",
+    "por": "Olá",
+    "spa": "Hola",
+    "swa": "Habari",
+    "zho": "你好",
+}
+
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', greeting='Hello')
+    language = app.config.get("LANGUAGE", "eng")
+    return flask.render_template('index.html', greeting=TRANSLATIONS[language])
 
 
 if __name__ == '__main__':
