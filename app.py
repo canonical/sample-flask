b24b17cfdee863091b678c781a8eaf3932539797
Weii Wang <weii.wang@canonical.com>
2024-01-17 14:54:49 +0800

    Add visitor database

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import datetime
+import os
+
 import flask
-from flask import Flask
+import psycopg2
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config.from_prefixed_env()
@@ -15,11 +19,37 @@ TRANSLATIONS = {
     "zho": "你好",
 }
 
+DATABASE_URI = os.environ["POSTGRESQL_DB_CONNECT_STRING"]
+
 
 @app.route('/')
 def index():
+    with psycopg2.connect(DATABASE_URI) as conn, conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS visitors (
+                timestamp TIMESTAMP NOT NULL,
+                user_agent TEXT NOT NULL
+            );
+        """)
+
+        user_agent = request.headers.get('User-Agent')
+        timestamp = datetime.datetime.now()
+        cur.execute(
+            "INSERT INTO visitors (timestamp, user_agent) VALUES (%s, %s)",
+            (timestamp, user_agent)
+        )
+
+        cur.execute("SELECT COUNT(*) FROM visitors")
+        total_visitors = cur.fetchone()[0]
+
+        conn.commit()
+
     language = app.config.get("LANGUAGE", "eng")
-    return flask.render_template('index.html', greeting=TRANSLATIONS[language])
+    return flask.render_template(
+        'index.html',
+        greeting=TRANSLATIONS[language],
+        visitors=total_visitors
+    )
 
 
 if __name__ == '__main__':
